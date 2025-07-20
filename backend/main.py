78a9be911d4808675d3a54c2c9e47f46dca59ec3
Srinivasa <srinivasaperisetla@gmail.com>
2025-07-19 22:34:27 -0700

    removed main method

--- backend/main.py
+++ backend/main.py
@@ -75,5 +75,5 @@ def predict(input_data: TestModelPredictionInput):
 
 # for local use only
 
-if __name__ == "__main__":
-  uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+# if __name__ == "__main__":
+#   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
